9e99c6b4c6fb3c922cab9c9eb3e1225628333135
Radina Avramova <99686592+RadinaAvramova@users.noreply.github.com>
2024-06-23 20:33:27 +0300

    Update Welcome.vue

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -287,7 +287,6 @@ defineProps({
                 <div class="text-center text-sm text-gray-500 dark:text-gray-400 sm:text-left">
                     <div class="flex items-center gap-4">
                         <a
-                            href="https://github.com/sponsors/taylorotwell"
                             class="group inline-flex items-center hover:text-gray-700 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
                         >
                             <svg
